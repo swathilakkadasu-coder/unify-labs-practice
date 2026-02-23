f473e63734f67ed62783501ba15f099ffef9e66e
swathilakkadasu-coder <swathilakkadasu@gmail.com>
2026-02-23 21:05:19 +0530

    🚀 Day 27 - index.html - 9:04:35 pm

---
+++ Day-27/script.js
@@ -0,0 +1,7 @@
+// Day 27 Logic Practice
+console.log('Lab Session 27 Started');
+
+const display = document.getElementById('display');
+display.innerText = 'Logic Engine Online';
+
+// Practice your JS code here...
